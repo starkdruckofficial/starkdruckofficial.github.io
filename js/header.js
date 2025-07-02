6bf5720a98b92815404b0548eab24d2bd26c9d02
Alex <starkal91704@th-nuernberg.de>
2025-07-02 13:20:10 +0200

    First Website Input

---
+++ js/header.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const toggle = document.getElementById("menu-toggle");
+    const nav = document.getElementById("nav-menu");
+
+    toggle.addEventListener("click", function () {
+        nav.classList.toggle("show");
+    });
+});
